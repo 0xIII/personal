cc58e37b44fb91181a46c31f03003f2d50e89496
0xIII <louiskaran@gmail.com>
2023-05-25 12:28:01 +0200

    Overhaul

--- src/io.rs
+++ src/io.rs
@@ -1,15 +1,14 @@
-use std::fs::{DirEntry, File, read_dir, ReadDir, create_dir, create_dir_all};
-use std::io::{prelude::*, Result, Lines, BufReader};
+use std::fs::{create_dir, create_dir_all, read_dir, DirEntry, File, ReadDir};
+use std::io::{prelude::*, BufReader, Lines, Result};
 use std::path::{Path, PathBuf};
 
 pub fn list_files(dir: &str) -> Vec<PathBuf> {
     let mut pathes: Vec<PathBuf> = Vec::new();
-    let files = read_dir(dir)
-        .unwrap_or_else(|_| {
-            println!("{dir} not found! Creating {dir}", dir=dir);
-            create_dir(dir).unwrap();
-            read_dir(dir).unwrap()
-        });
+    let files = read_dir(dir).unwrap_or_else(|_| {
+        println!("{dir} not found! Creating {dir}", dir = dir);
+        create_dir(dir).unwrap();
+        read_dir(dir).unwrap()
+    });
     for path in files {
         pathes.push(path.unwrap().path());
     }
@@ -18,26 +17,23 @@ pub fn list_files(dir: &str) -> Vec<PathBuf> {
 
 pub fn read_string<P: AsRef<Path>>(filename: P) -> Result<String> {
     let mut buffer = String::new();
-    let mut handle = File::open(&filename).unwrap_or_else(|_| {
-        File::create(&filename).expect("Unable to create file")
-    });
+    let mut handle = File::open(&filename)
+        .unwrap_or_else(|_| File::create(&filename).expect("Unable to create file"));
     handle.read_to_string(&mut buffer)?;
     Ok(buffer)
 }
 
 pub fn read_vec<P: AsRef<Path>>(filename: P) -> Result<Vec<u8>> {
     let mut buffer = Vec::new();
-    let mut handle = File::open(&filename).unwrap_or_else(|_| {
-        File::create(&filename).expect("Unable to create file")
-    });
+    let mut handle = File::open(&filename)
+        .unwrap_or_else(|_| File::create(&filename).expect("Unable to create file"));
     handle.read_to_end(&mut buffer)?;
     Ok(buffer)
 }
 
 pub fn lines<P: AsRef<Path>>(filename: P) -> Result<Lines<BufReader<File>>> {
-    let handle = File::open(&filename).unwrap_or_else(|_| {
-        File::create(&filename).expect("Unable to create file")
-    });
+    let handle = File::open(&filename)
+        .unwrap_or_else(|_| File::create(&filename).expect("Unable to create file"));
     Ok(BufReader::new(handle).lines())
 }
 
@@ -48,7 +44,7 @@ pub trait Transform {
 pub enum Transforms {
     Lowercase,
     Uppercase,
-    NoWhitespaces
+    NoWhitespaces,
 }
 
 impl Transform for String {
@@ -57,14 +53,12 @@ impl Transform for String {
             Transforms::Lowercase => {
                 self.make_ascii_lowercase();
                 self
-            },
+            }
             Transforms::Uppercase => {
                 self.make_ascii_uppercase();
                 self
-            },
-            Transforms::NoWhitespaces => {
-                self.replace(" ", "-")
             }
+            Transforms::NoWhitespaces => self.replace(" ", "-"),
         }
     }
 }
